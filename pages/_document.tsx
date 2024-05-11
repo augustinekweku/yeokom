8291bdc6d46f40c102f763ebf4970d84df00b7e8
Augustine Nani <augustine.nani@hubtel.com>
2024-05-11 19:42:53 +0000

    add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="wide wow-animation" lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href={"/img/logo-2.png"} type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
