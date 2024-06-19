69a493137b94edfce11bec4b8c866b697be6f4de
Augustine Nani <augustine.nani@hubtel.com>
2024-06-19 08:32:24 +0000

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,15 +29,19 @@ const Navbar: FC<Props> = ({ customHeaderWrapperClass }) => {
   const router = useRouter();
 
   function isLinkActive(href: string) {
-    console.log(router.asPath, href);
     const isLinkActive = router.asPath === href;
     return isLinkActive ? "active" : "";
   }
   return (
     <header className={`header-area ${customHeaderWrapperClass}`}>
-      <Link href="/" className="logo-area">
+      <a
+        onClick={() => {
+          location.href = "/";
+        }}
+        className="logo-area"
+      >
         <img src="img/logo-2.png" width={140} alt="" />
-      </Link>
+      </a>
       <div className="nav-switch">
         <i className="fa fa-bars"></i>
       </div>
@@ -46,7 +50,7 @@ const Navbar: FC<Props> = ({ customHeaderWrapperClass }) => {
         <ul>
           {NAV_LINKS.map((link) => (
             <li key={link.title} className={isLinkActive(link.href)}>
-              <Link href={link.href}>{link.title}</Link>
+              <a href={link.href}>{link.title}</a>
             </li>
           ))}
         </ul>
