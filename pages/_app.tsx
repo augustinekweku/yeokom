59debad4af1ba579d980153576495742ec3e8af1
Augustine Nani <augustine.nani@hubtel.com>
2024-06-20 15:27:56 +0000

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import "../public/css/bootstrap.min.css";
 import "../public/css/font-awesome.min.css";
 import "../public/style.scss";
 import CustomScript from "@/components/CustomScript";
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
